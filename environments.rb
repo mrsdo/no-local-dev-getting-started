0b247fc43887ab9dea6dd9c5d1f564aa6a9e47f5
Marti D <me@martidolce.com>
2021-01-31 02:54:20 -0500

    Update environments.rb

--- environments.rb
+++ environments.rb
@@ -7,9 +7,11 @@ configure :development, :production do
 end
 
 configure :production, :development do
- db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+  # db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
- ActiveRecord::Base.establish_connection(
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://lvbsyipxlnjzvf:db94c7c5f909a597735ac3d609b0473070dee24ab111025ab7ffd4dc06100cc2@ec2-75-101-212-64.compute-1.amazonaws.com:5432/d1stml5b21fb20')
+
+  ActiveRecord::Base.establish_connection(
    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
    :host     => db.host,
    :username => db.user,
