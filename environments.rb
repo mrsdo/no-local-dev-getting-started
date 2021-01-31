738dc43c1e063ad6e98a6ad1ef7d073dae39b7c5
Marti D <me@martidolce.com>
2021-01-31 03:50:01 -0500

    updating database.yml

--- environments.rb
+++ environments.rb
@@ -7,25 +7,15 @@ configure :development, :production do
 end
 
 configure :production, :development do
-  # db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://lvbsyipxlnjzvf:db94c7c5f909a597735ac3d609b0473070dee24ab111025ab7ffd4dc06100cc2@ec2-75-101-212-64.compute-1.amazonaws.com:5432/d1stml5b21fb20')
-
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => 'ec2-75-101-212-64.compute-1.amazonaws.com',
-    :username => 'lvbsyipxlnjzvf',
-    :password => 'db94c7c5f909a597735ac3d609b0473070dee24ab111025ab7ffd4dc06100cc2',
-    :database => './db/database.yml',
-    :encoding => 'utf8'
-
-  # ActiveRecord::Base.establish_connection(
-  #  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  #  :host     => db.host,
-  #  :username => db.user,
-  #  :password => db.password,
-  #  :database => db.path[1..-1],
-  #  :encoding => 'utf8'
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
  )
 end
 
